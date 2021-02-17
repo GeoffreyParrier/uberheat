4911507a043509fe50eb9686585d27fd2787744b
Julien SEIXAS (MrZyr0) <>
2021-02-17 15:42:31 +0100

    fix: CircProductConfiguration typo error

--- app/Models/CircProductConfiguration.php
+++ app/Models/CircProductConfiguration.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class CircProductCondiguration extends ProductConfiguration
+class CircProductConfiguration extends ProductConfiguration
 {
     use HasFactory;
 

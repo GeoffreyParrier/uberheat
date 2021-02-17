5b30865eadec96cfabfe6ca35b113f7f19bf2101
Julien SEIXAS (MrZyr0) <>
2021-02-17 15:39:18 +0100

    feat: create CircProductCondiguration model with it migration

---
+++ app/Models/CircProductCondiguration.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class CircProductCondiguration extends ProductConfiguration
+{
+    use HasFactory;
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'width',
+        'height',
+        'thickness',
+    ];
+
+    /**
+     * @return float
+     */
+    public function getSurface(): float
+    {
+        return $this->width * $this->height;
+    }
+}
---
+++ database/migrations/2021_02_17_140900_create_circproductconfigurations_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCircProductConfigurationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('circproductconfigurations', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->float('depth');
+            $table->float('db_1');
+            $table->float('db_2');
+            $table->float('db_5');
+            $table->float('db_10');
+            $table->float('width');
+            $table->float('height');
+            $table->float('thickness');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('circ_product_condigurations');
+    }
+}

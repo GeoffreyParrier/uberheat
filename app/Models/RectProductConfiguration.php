706875186c77f8773349a2171a2d8ae5e10b95c3
Julien SEIXAS (MrZyr0) <>
2021-02-17 15:38:57 +0100

    feat: create RectProductConfiguration model with it migration

---
+++ app/Models/RectProductConfiguration.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+
+class RectProductConfiguration extends ProductConfiguration
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'diameter',
+    ];
+
+    /**
+     * @return float
+     */
+    public function getSurface(): float
+    {
+        return $this->depth * $this->diameter;
+    }
+}

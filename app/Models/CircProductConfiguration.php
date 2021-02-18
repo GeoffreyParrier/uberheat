f8ed3052bfa5684b9090b5a25dba8dd0446094c1
Julien SEIXAS (MrZyr0) <>
2021-02-18 17:47:56 +0100

    fix: models inheritance

--- app/Models/CircProductConfiguration.php
+++ app/Models/CircProductConfiguration.php
@@ -2,29 +2,33 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
+/**
+ * Class CircProductConfiguration
+ * @package App\Models
+ * @mixin Builder
+ */
 class CircProductConfiguration extends ProductConfiguration
 {
     use HasFactory;
 
+    public function __construct(array $attributes = [])
+    {
+        $fillable = [
+            'diameter',
+        ];
+        $this->fillable = array_merge($fillable, $this->fillable);
+        parent::__construct($attributes);
+    }
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'width',
-        'height',
-        'thickness',
-    ];
 
     /**
      * @return float
      */
     public function getSurface(): float
     {
-        return $this->width * $this->height;
+        return $this->depth * $this->diameter;
     }
 }
--- app/Models/RectProductConfiguration.php
+++ app/Models/RectProductConfiguration.php
@@ -2,28 +2,37 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 
+/**
+ * Class RectProductConfiguration
+ * @package App\Models
+ * @mixin Builder
+ */
 class RectProductConfiguration extends ProductConfiguration
 {
     use HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'diameter',
-    ];
+    public function __construct(array $attributes = [])
+    {
+        $fillable = [
+            'width',
+            'height',
+            'thickness',
+        ];
+        $this->fillable = array_merge($fillable, $this->fillable);
+        parent::__construct($attributes);
+    }
+
 
     /**
      * @return float
      */
     public function getSurface(): float
     {
-        return $this->depth * $this->diameter;
+        return $this->width * $this->height;
     }
 
 }

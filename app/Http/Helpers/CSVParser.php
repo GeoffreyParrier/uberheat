abdd6f94d8e81b63e417f3ff707ec1023433aad5
Julien SEIXAS (MrZyr0) <>
2021-02-18 17:48:33 +0100

    fix: CSVPraser returned data

--- app/Http/Helpers/CSVParser.php
+++ app/Http/Helpers/CSVParser.php
@@ -13,12 +13,20 @@ class CSVParser
     {
         $response = [];
         $var = explode("\n", $file->getContent());
+
         $columns_name = explode(";", $var[0]);
+        for ($i = 0; $i < count($columns_name); $i++) {
+            $columns_name[$i] = trim($columns_name[$i]);
+        }
+
+
         array_shift($var);
+        array_pop($var);
+
         foreach ($var as $row) {
             $column_array = [];
             foreach (explode(";", $row) as $index => $column) {
-                $column_array[$columns_name[$index]] = $column;
+                $column_array[$columns_name[$index]] = trim($column);
             }
             array_push($response, $column_array);
         }

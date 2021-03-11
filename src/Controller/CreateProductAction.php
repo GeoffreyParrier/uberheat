10fa57d15aa419e05e3ff4fa8be715667ed3a465
Julien SEIXAS (MrZyr0) <>
2021-03-11 13:21:59 +0100

    feat: implement product configuration into product create route

--- src/Controller/CreateProductAction.php
+++ src/Controller/CreateProductAction.php
@@ -5,12 +5,13 @@ namespace App\Controller;
 
 use App\Entity\MediaObject;
 use App\Entity\Product;
+use App\Repository\ProductConfigurationRepository;
 use Symfony\Component\HttpFoundation\Exception\BadRequestException;
 use Symfony\Component\HttpFoundation\Request;
 
 final class CreateProductAction
 {
-    public function __invoke(Request $request): Product
+    public function __invoke(Request $request, ProductConfigurationRepository $productConfigurationRepository): Product
     {
         $name = $request->get('name');
         if (!$name) {
@@ -27,6 +28,11 @@ final class CreateProductAction
             throw new BadRequestException('Product picture is required');
         }
 
+        $configurations_ids = explode(', ', $request->get('configurations_ids'));
+        if (!$configurations_ids) {
+            throw new BadRequestException('Product configurations_ids is required');
+        }
+
         $mediaObject = new MediaObject();
         $mediaObject->file = $picture;
 
@@ -34,6 +40,11 @@ final class CreateProductAction
         $product->setName($name)
             ->setBasePrice($basePrice)
             ->setProductImg($mediaObject);
+
+        foreach ($configurations_ids as $configuration_id) {
+            $configurationObj = $productConfigurationRepository->find($configuration_id);
+            $product->addConfiguration($configurationObj);
+        }
 
         return $product;
     }
